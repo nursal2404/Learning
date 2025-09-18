c94ce09b85d14aca3423630fd21cda51b34e97ce
nursal2404 <muhadzinur@gmail.com>
2025-09-18 13:41:50 +0700

    Create Base Project Spring

---
+++ Spring/src/test/java/Learning/intro/IntroApplicationTests.java
@@ -0,0 +1,13 @@
+package Learning.intro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
